95e8e67d71c22763c713a67d7d9ffedf085f1112
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-17 17:01:58 +0530

    ADDED:Line comparison Equal Greater and Less

--- src/UC3_EqualGreaterLess.java
+++ src/UC3_EqualGreaterLess.java
@@ -1,7 +1,7 @@
-import java.lang.*;
 import java.util.Objects;
+import java.lang.*;
 
-public class UC2_QualityOfTwoLines {
+public class UC3_EqualGreaterLess {
     static double length(int x1, int x2, int y1, int y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) * 1.0);
     }
@@ -14,12 +14,15 @@ public class UC2_QualityOfTwoLines {
         System.out.println("Welcome To Line Comparison Computation Program");
         Double line1 = Math.round(length(5, 7, 8, 9)) * 100000.0 / 100000.0;
         Double line2 = Math.round(length2(5, 7, 8, 9)) * 100000.0 / 100000.0;
-        //if (Objects.equals(line2,line1))
-        // {
-        System.out.println("The Equality of two Lines is Equal " + (line1.equals(line2)));
-        //}
-        //else {
-        //System.out.println(line1+ " line1 Are Not Equal to line2 " + line2 );
-        //}
+        System.out.println("The Equality of two Lines is Equal = " + (Objects.equals(line2, line1)));
+        int result = line1.compareTo(line2);
+        if (result == 0) {
+            System.out.println("line1 and line to are equal = " + result);
+        } else if (result > 0) {
+            System.out.println("line1 is greater than line2 = " + result);
+        } else
+            System.out.println("Line1 is less than line2 = " + result);
+
     }
 }
+
